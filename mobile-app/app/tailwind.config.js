cd2bbd839aa9fa0e64a8c0e440157d683fd14f29
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-05 12:33:10 +0900

    install tailwind

---
+++ mobile-app/app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "../app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
