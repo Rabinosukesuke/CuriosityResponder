29a2ccaabf925616948b5bbfcb8af8e8319ae283
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-06 20:25:33 +0900

    style HomeScreen

--- mobile-app/app/screens/HomeScreen.tsx
+++ mobile-app/app/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import React from 'react';
 import { UserInfo } from '../components/UserInfo';
 import { useSelector } from 'react-redux';
@@ -6,49 +6,99 @@ import { selectUser } from '../slices/userSlices';
 import { useDispatch } from 'react-redux';
 import { signOut } from '../slices/userSlices';
 import { AppDispatch } from '../store';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/type';
 
-export const HomeScreen = ({ navigation }: any) => {
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Home">;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+}
+
+export const HomeScreen = ({ navigation }: Props) => {
 
   const dispatch = useDispatch<AppDispatch>();
 
   const user = useSelector(selectUser);
 
   return (
-    <View style={styles.container}>
-      {user?.user ? (
-        <View>
-          <Text>You are already logged in</Text>
-          <UserInfo />
-          <Button
-            title='Sign Out'
-            onPress={() => {
-              dispatch(signOut())
-            }}
-          />
-        </View>
-      ) : (
-        <View >
-          <Text>HomeScreen</Text>
-          <Button
-            title='Sign In'
-            onPress={() => navigation.navigate("SignIn")}
-          />
-          <Button
-            title='Sign Up'
-            onPress={() => navigation.navigate("SignUp")}
-          />
-        </View>
-      )}
-    </View>
+    <View className='bg-primary flex-1'>
+      {
+        user?.user ? (
+          // logged in
+          <View className='flex-1 items-center justify-center'>
+            <Text className='text-3xl'>
+              You are already logged in
+            </Text>
+
+            <UserInfo />
+
+            {/* SignOut Button */}
+            <Pressable
+              style={styles.button}
+              onPress={() => {
+                dispatch(signOut())
+              }}>
+              <Text
+                style={styles.buttonText}
+              >ログアウト</Text>
+            </Pressable>
+
+
+          </View>
+        ) : (
+          // not logged in
+          <View className='flex-1 items-center justify-center'>
+            {/* SignIn Button */}
+            <Pressable
+              style={styles.button}
+              onPress={() => navigation.navigate("SignIn")}
+            >
+              <Text
+                style={styles.buttonText}
+              >ログイン</Text>
+            </Pressable >
+
+            {/* SignUp Button */}
+            <Pressable
+              style={styles.button}
+              onPress={() => navigation.navigate("SignUp")}
+            >
+              <Text
+                style={styles.buttonText}
+              >アカウント登録</Text>
+            </Pressable >
+
+          </View >
+        )
+      }
+    </View >
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  button: {
+    width: "70%",
+    height: "5%",
+    borderWidth: 1,
+    shadowColor: "#000",
+    backgroundColor: "#6EE7B3",
+    flexDirection: "column",
+    borderRadius: 50,
+    borderColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 1,
+    marginVertical: 5,
   },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    // fontFamily: "Junge",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: "400",
+    letterSpacing: -0.5,
+    textTransform: "capitalize",
+  }
 });
-
