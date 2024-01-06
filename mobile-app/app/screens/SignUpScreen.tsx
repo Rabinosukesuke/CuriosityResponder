8f1d8b932ffb63f85c6623c4233b93fa04573523
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-06 20:26:01 +0900

    style SignUpScreen

--- mobile-app/app/screens/SignUpScreen.tsx
+++ mobile-app/app/screens/SignUpScreen.tsx
@@ -1,49 +1,85 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { Button, Input } from 'react-native-elements';
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { Input } from 'react-native-elements';
 import React, { useState } from 'react'
-import { UserInfo } from '../components/UserInfo'
 import { useDispatch } from 'react-redux';
 import { signUp } from '../slices/userSlices'
 import { AppDispatch } from '../store';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/type';
 
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "SignUp">;
 
-export const SignUpScreen = ({ navigation }: any) => {
+type Props = {
+    navigation: HomeScreenNavigationProp;
+}
+
+export const SignUpScreen = ({ navigation }: Props) => {
     const dispatch = useDispatch<AppDispatch>();
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
     return (
-        <View style={styles.container}>
-            <Text>SignUpScreen</Text>
+        <View className='bg-primary flex-1 items-center justify-center'>
+            <Text className='text-3xl font-bold'>アカウント登録</Text>
             <Input
                 placeholder='type email'
                 value={email}
                 onChangeText={setEmail}
+                textContentType='emailAddress'
             />
             <Input
                 placeholder='type password'
                 value={password}
                 onChangeText={setPassword}
+                textContentType='password'
             />
-            <Button title="LogUp" onPress={() => {
-                dispatch(signUp({ email, password }))
-                    .then(() => {
-                        navigation.navigate("Home")
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-            }} />
+
+            {/* SignUp Button */}
+            <Pressable
+                style={styles.button}
+                onPress={() => {
+                    dispatch(signUp({ email, password }))
+                        .then(() => {
+                            navigation.navigate("Home")
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        })
+                }}>
+                <Text
+                    style={styles.buttonText}>
+                    登録
+                </Text>
+            </Pressable>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    button: {
+        width: "70%",
+        height: "5%",
+        borderWidth: 1,
+        shadowColor: "#000",
+        backgroundColor: "#6EE7B3",
+        flexDirection: "column",
+        borderRadius: 50,
+        borderColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 1,
+        marginVertical: 5,
     },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+        // fontFamily: "Junge",
+        fontSize: 20,
+        fontStyle: "normal",
+        fontWeight: "400",
+        letterSpacing: -0.5,
+        textTransform: "capitalize",
+    }
 });
