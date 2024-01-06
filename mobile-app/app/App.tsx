1457458da56d77e046e83bba70043a42a66cd9b9
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-06 14:23:54 +0900

    NativeWindのスタイルシートの出力を設定

--- mobile-app/app/App.tsx
+++ mobile-app/app/App.tsx
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { RouteNavigator } from './navigations/RouteNavigator';
 import { Provider } from "react-redux";
 import { store } from './store';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
