2cab353b8413a3c51ed3382e9212d06593daff84
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-06 11:40:22 +0900

    Update babel.config.js

--- mobile-app/app/babel.config.js
+++ mobile-app/app/babel.config.js
@@ -3,8 +3,11 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     "plugins": [
-      ["module:react-native-dotenv"]
+      ['module:react-native-dotenv', {
+        "moduleName": "@env",
+        "path": ".env.local",
+      }],
+      ['nativewind/babel'],
     ],
-    plugins: ['nativewind/babel'],
   };
 };
