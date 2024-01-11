7cbd1206b6cebe689c6f36be137ecd2abd86fdf1
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-11 22:02:23 +0900

    add type StorageData

--- mobile-app/app/types/type.ts
+++ mobile-app/app/types/type.ts
@@ -3,19 +3,21 @@ export type RootStackParamList = {
     SignIn: undefined;
     SignUp: undefined;
     ParentLogin: undefined;
-    ChildChatScreen: undefined; 
-    ChildHistoryScreen: undefined; 
-    MediaInputScreen: undefined; 
-
-};
-
-export type EmailWithPassword = {
-    email: string;
-    password: string;
+    ChildChatScreen: undefined;
+    ChildHistoryScreen: undefined;
+    MediaInputScreen: undefined;
 
 };
 
 export type User = {
     uid: string;
     email: string | null;
+}
+
+export type StorageData = {
+    // key: string;
+    timestamp: Date;
+    question: string;
+    answer: string;
+    emoji: "happy" | "normal" | "sad";
 }
