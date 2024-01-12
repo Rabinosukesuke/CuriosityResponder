9ca3414cd55dc4b200d72eba2bc96f36f17d5880
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-12 19:28:08 +0900

    Local storageの初期データの更新

--- mobile-app/app/initialStrageData.ts
+++ mobile-app/app/initialStrageData.ts
@@ -1,19 +1,19 @@
-import { StorageData } from './types/type';
-export const initialStorageData: Array<StorageData> = [
+import { ChatData } from './types/type';
+export const initialStorageData: Array<ChatData> = [
     {
-        timestamp: new Date(),
+        timestamp: new Date(2024, 1, 1),
         question: "1st question",
         answer: "1st answer",
         emoji: "happy",
     },
     {
-        timestamp: new Date(),
+        timestamp: new Date(2023, 1, 1),
         question: "2nd question",
         answer: "2nd answer",
         emoji: "normal",
     },
     {
-        timestamp: new Date(),
+        timestamp: new Date(2022, 1, 1),
         question: "3rd question",
         answer: "3rd answer",
         emoji: "sad",
