d7c78397825e9214edbe3b17fe4a03e2f31a1c99
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2024-01-13 22:01:34 +0900

    backendに使用する環境変数の定義

--- backend/app/lambda_function.py
+++ backend/app/lambda_function.py
@@ -1,7 +1,12 @@
+import os
 import json
 
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    # env
+    aws_region: str = os.environ["AWS_REGION"]
+    aws_access_key_id: str = os.environ["AWS_ACCESS_KEY_ID"]
+    aws_secret_access_key: str = os.environ["AWS_SECRET_ACCESS_KEY"]
+    dynamodb_qa_table_name: str = os.environ["DYNAMODB_QA_TABLE_NAME"]
+
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
