b63cf05d78405d13efd338f85d3258182402b401
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-14 22:14:49 +0900

    app.tsxでフォントのロードを行うように変更

--- mobile-app/app/App.tsx
+++ mobile-app/app/App.tsx
@@ -1,20 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteNavigator } from './navigation/RouteNavigator';
 import { Provider } from "react-redux";
 import { store } from './store';
 import { NativeWindStyleSheet } from "nativewind";
 import { useChatStorage } from './hooks/useChatStorage';
+import * as Font from 'expo-font';
+import { Yellowtail_400Regular } from '@expo-google-fonts/dev';
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
   const { initChatDataInStorage } = useChatStorage();
 
   useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        Yellowtail_400Regular,
+      });
+      setFontsLoaded(true);
+    }
     initChatDataInStorage();
+    loadFonts();
   }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <Provider store={store}>
