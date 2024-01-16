5089ab1738d67f13adb170a768163922cc5b7047
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-16 19:48:13 +0900

    フロント環境変数にBACKEND_URLとBACKEND_API_KEYの追加

--- mobile-app/app/types/env.d.ts
+++ mobile-app/app/types/env.d.ts
@@ -6,4 +6,6 @@ declare module "@env" {
     export const FIREBASE_MESSAGING_SENDER_ID: string;
     export const FIREBASE_APP_ID: string;
     export const OPENAI_API_KEY: string;
+    export const BACKEND_URL: string;
+    export const BACKEND_API_KEY: string;
 }
