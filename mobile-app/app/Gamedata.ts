cdd74425ca33cfbeaba0f3bf7409f39885bc99d1
takedayuto1 <20122051@kaishi-pu.ac.jp>
2024-01-20 12:11:57 +0900

    Gamedata.jsの削除とGamedata.tsの追加

--- mobile-app/app/Gamedata.ts
+++ mobile-app/app/Gamedata.ts
@@ -1,6 +1,5 @@
-
-export const questions = [
+export const questions: { id: number; question: string; answer: string }[] = [
     { id: 1, question: '私は動物です。', answer: '犬' },
     { id: 2, question: '私は有名な人物です。', answer: 'アインシュタイン' },
     // 他にも質問と答えのペアを追加
-  ];
+];
