292147d11b5158f0bbffa247272a59fdde97a974
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-20 18:59:47 +0900

    DailyHistory画面に履歴の表示を追加

--- mobile-app/app/screens/DailyHistory.tsx
+++ mobile-app/app/screens/DailyHistory.tsx
@@ -1,9 +1,13 @@
-import { View, Text, FlatList, Pressable, StyleSheet } from 'react-native'
+import { View, Text, FlatList, Pressable, StyleSheet, ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../types/type';
+import { RootStackParamList, ChatData } from '../types/type';
 import { Header } from '../components/Header'
 import { FloatingButton } from '../components/FloatingActionButton';
+import { useSelector } from 'react-redux'
+import { selectAuth } from '../slices/authSlices';
+import { ChildHistoryComponent } from '../components/ChildHistoryComponent';
+import { useBackendAPI } from '../hooks/useBackendAPI';
 
 type Props = {
     navigation: NativeStackNavigationProp<RootStackParamList, "DailyHistory">;
@@ -16,44 +20,79 @@ type DateWithDayOfWeek = {
     dayOfWeek: number
 }
 
+const getDaysFromNowToLastMonth = (): DateWithDayOfWeek[] => {
+    const today = new Date();
+    const lastMonth = new Date();
+    lastMonth.setMonth(today.getMonth() - 1);
+
+    const dateWithDayOfWeekList = [];
+    while (today >= lastMonth) {
+        const dateWithDayOfWeek: DateWithDayOfWeek = {
+            datetime: new Date(today),
+            month: today.getMonth() + 1,
+            day: today.getDate(),
+            dayOfWeek: today.getDay()
+        }
+        dateWithDayOfWeekList.push(dateWithDayOfWeek);
+        today.setDate(today.getDate() - 1);
+    }
+    console.log("function")
+
+    return dateWithDayOfWeekList.reverse();
+}
+
 export const DailyHistory = ({ navigation }: Props) => {
+    const user = useSelector(selectAuth);
+
     const dayOfWeekList = ["日", "月", "火", "水", "木", "金", "土"];
     const nowMonth = new Date().getMonth() + 1;
     const nowDay = new Date().getDate();
-    
-    const getDaysFromNowToLastMonth = (): DateWithDayOfWeek[] => {
-        const today = new Date();
-        const lastMonth = new Date();
-        lastMonth.setMonth(today.getMonth() - 1);
-
-        const dateWithDayOfWeekList = [];
-        while (today >= lastMonth) {
-            const dateWithDayOfWeek: DateWithDayOfWeek = {
-                datetime: new Date(today),
-                month: today.getMonth() + 1,
-                day: today.getDate(),
-                dayOfWeek: today.getDay()
-            }
-            dateWithDayOfWeekList.push(dateWithDayOfWeek);
-            today.setDate(today.getDate() - 1);
-        }
-
-        return dateWithDayOfWeekList.reverse();
-    }
-    const dateWithDayOfWeekList = getDaysFromNowToLastMonth();
-
-    useEffect(() => {
-        setSelectedDate(dateWithDayOfWeekList.length - 1);
-    }, [dateWithDayOfWeekList]);
 
     const [selectedDate, setSelectedDate] = useState<number>(0);
+    const [storageData, setStorageData] = useState<ChatData[]>([]);
+    const [isToggled, setIsToggled] = useState<boolean>(false);
+
+    const dateWithDayOfWeekList = getDaysFromNowToLastMonth();
+
+    const { periodFetchChatHistoryFromBackend } = useBackendAPI();
+
+    const lastIndex = dateWithDayOfWeekList.length - 1
+
+    const fetchData = async (index: number) => {
+        if (user == null) {
+            console.log("user is null");
+            navigation.navigate("Home");
+        } else {
+            const start = new Date(dateWithDayOfWeekList[index].datetime.setHours(0, 0, 0, 0));
+            const end = new Date(dateWithDayOfWeekList[index].datetime.setHours(23, 59, 59, 999));
+            const data = await periodFetchChatHistoryFromBackend(user.uid, start, end);
+            setStorageData(data);
+        }
+    };
+
+    useEffect(() => {
+        setSelectedDate(lastIndex);
+        fetchData(lastIndex);
+        console.log("useEffect")
+    }, []);
+
+    const SortChatData = storageData
+        .sort((a, b) => {
+            if (isToggled) {
+                return new Date(a.datetime) > new Date(b.datetime) ? 1 : -1;
+            } else {
+                return new Date(a.datetime) < new Date(b.datetime) ? 1 : -1;
+            }
+        });
 
     const renderDateWithDayOfWeek = ({ item, index }: { item: DateWithDayOfWeek, index: number }) => (
         <Pressable
-            className='flex-1 items-center justify-center w-12 mx-1 my-5 '
-            onPress={() => {
+            className='flex-1 items-center justify-center w-12 mx-1 my-5'
+            onPress={async () => {
                 console.log("pressed", item.day)
                 setSelectedDate(index)
+                await fetchData(index);
+                console.log("fetchData", storageData)
             }}
         >
             {index == selectedDate ? (
@@ -94,9 +133,9 @@ export const DailyHistory = ({ navigation }: Props) => {
 
     return (
         <View className='bg-primary flex-1'>
-            <Header navigation={navigation}/>
+            <Header navigation={navigation} />
 
-            <View className='h-30'>
+            <View style={{ width: "100%", height: "10%" }}>
                 <FlatList
                     data={dateWithDayOfWeekList}
                     renderItem={renderDateWithDayOfWeek}
@@ -108,10 +147,24 @@ export const DailyHistory = ({ navigation }: Props) => {
                     )}
                 />
             </View>
+
+            <ScrollView
+                style={{ width: "100%", paddingTop: "5%", overflow: "scroll" }}
+            >
+                {SortChatData.map((item: ChatData) => (
+                    <ChildHistoryComponent
+                        key={item.datetime.toString()}
+                        datetime={item.datetime}
+                        question={item.question}
+                        answer={item.answer}
+                        emoji={item.emoji}
+                    />
+                ))}
+            </ScrollView>
+
             <View style={styles.floatingButtonContainer}>
                 <FloatingButton />
             </View>
-
         </View>
     )
 }
